c704538d88b8b117d088963d0347e73e81b037c1
Omer Lande <omerl@log-on.com>
2025-03-18 17:00:28 +0200

    added error message for empty task

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Modal from "./Modal";
 
 export default function NewTask({ onAdd }) {
   const [enteredTask, setEnteredTask] = useState("");
+  const modal = useRef();
 
   function handleChange(event) {
     setEnteredTask(event.target.value);
@@ -9,6 +11,7 @@ export default function NewTask({ onAdd }) {
 
   function handleClick() {
     if (enteredTask.trim() === "") {
+      modal.current.open();
       return;
     }
     onAdd(enteredTask);
@@ -16,19 +19,30 @@ export default function NewTask({ onAdd }) {
   }
 
   return (
-    <div className="flex items-center gap-4">
-      <input
-        type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
-        onChange={handleChange}
-        value={enteredTask}
-      />
-      <button
-        className="text-stone-700 hover:text-stone-950"
-        onClick={handleClick}
-      >
-        Add Task
-      </button>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          looks like atleast 1 value is not filled
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please fill the tasks you want to do!
+        </p>
+      </Modal>
+      <div className="flex items-center gap-4">
+        <input
+          type="text"
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+          onChange={handleChange}
+          value={enteredTask}
+        />
+        <button
+          className="text-stone-700 hover:text-stone-950"
+          onClick={handleClick}
+        >
+          Add Task
+        </button>
+      </div>
+    </>
   );
 }
