1d14b8390a42da774361b6227686a99f039a7cc2
Omer Lande <omerl@log-on.com>
2025-03-18 11:51:53 +0200

    added projects to the sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,18 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prev) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prev,
+        selectedProjectId: undefined,
         projects: [...prev.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
   if (projectsState.selectedProjectId === null) {
@@ -42,7 +42,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
