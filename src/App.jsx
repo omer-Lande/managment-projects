97aef9a02d56f05e940c225a16b9f1e7dce65e60
Omer Lande <omerl@log-on.com>
2025-03-18 11:43:23 +0200

    used ref for input new project props, saved project to an array

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,24 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prev) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prev,
+        projects: [...prev.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
